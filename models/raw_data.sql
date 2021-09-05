cfcf1f00230eb89538bf264e6e024bf5a8c5f2d0
Tarun-nano <nano.tarun@gmail.com>
2021-09-05 17:28:18 +0530

    test

--- models/raw_data.sql
+++ models/raw_data.sql
@@ -1,6 +1,20 @@
+{{ config(
+    materialized="table"
+) }}
+
 with source as (
   select * from students_data
 ),
+
+select
+  first_name,
+  last_name,
+from source
+
+
+
+
+
 
 
 select first_name,last_name 
